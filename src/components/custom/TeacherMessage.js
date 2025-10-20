648b76c2c31342a9be884db80a628166766d8366
saadi1212 <saadali6519@gmail.com>
2025-10-20 11:29:05 +0500

    created teacher message.js

---
+++ src/components/custom/TeacherMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from '../ui/index.js'; //import only Text from the UI layer
+
+export default function TeacherMessage() {
+  return (
+    <Text>
+      “Teacher Message: Perform Well!”
+      
+    </Text>
+   
+  );
+}
